685dcef9a93d4f3daa4a3611255f780db3e40529
jasminejeane <jasminejeane@gmail.com>
2018-05-02 08:13:32 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,110 @@
+package com.company;
+
+public class Main {
+
+
+//    Interactivity
+//void setup() {
+//    size(100, 100);
+//    noStroke();
+//}
+//
+//    void draw() {
+//        background(126);
+//        ellipse(mouseX, 16, 33, 33);    // Top circle
+//        ellipse(mouseX+20, 50, 33, 33); // Middle circle
+//        ellipse(mouseX-20, 84, 33, 33); // Bottom circle
+//    }
+
+//    void setup() {
+//        size(100, 100);
+//        strokeWeight(8);
+//    }
+//
+//    void draw() {
+//        background(204);
+//        line(mouseX, mouseY, pmouseX, pmouseY);
+//    }
+//
+//
+//    void setup() {
+//        size(100, 100);
+//        noStroke();
+//        fill(0);
+//    }
+//
+//    void draw() {
+//        background(204);
+//        if (mouseX < 50) {
+//            rect(0, 0, 50, 100); // Left
+//        } else {
+//            rect(50, 0, 50, 100); // Right
+//        }
+//    }
+
+
+//    void setup() {
+//        size(100, 100);
+//        noStroke();
+//        fill(0);
+//    }
+//
+//    void draw() {
+//        background(204);
+//        if (mouseX < 33) {
+//            rect(0, 0, 33, 100); // Left
+//        } else if (mouseX < 66) {
+//            rect(33, 0, 33, 100); // Middle
+//        } else {
+//            rect(66, 0, 33, 100); // Right
+//        }
+//    }
+
+
+//    int y = 35;
+//
+//    void setup() {
+//        size(100, 100);
+//    }
+//
+//    void draw() {
+//        background(204);
+//        line(10, 50, 90, 50);
+//        if (key == CODED) {
+//            if (keyCode == UP) {
+//                y = 20;
+//            } else if (keyCode == DOWN) {
+//                y = 50;
+//            }
+//        } else {
+//            y = 35;
+//        }
+//        rect(25, y, 50, 30);
+//    }
+
+
+//    boolean drawT = false;
+//
+//    void setup() {
+//        size(100, 100);
+//        noStroke();
+//    }
+//
+//    void draw() {
+//        background(204);
+//        if (drawT == true) {
+//            rect(20, 20, 60, 20);
+//            rect(39, 40, 22, 45);
+//        }
+//    }
+//
+//    void keyPressed() {
+//        if ((key == 'T') || (key == 't')) {
+//            drawT = true;
+//        }
+//    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
